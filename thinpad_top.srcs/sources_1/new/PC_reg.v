dbf51b2a0595ec7cd05a505e26a6c47298b376d5
王子奥 <wang-za19@mails.tsinghua.edu.cn>
2021-11-20 08:34:40 +0000

    Update PC_reg.v

--- thinpad_top.srcs/sources_1/new/PC_reg.v
+++ thinpad_top.srcs/sources_1/new/PC_reg.v
@@ -15,16 +15,14 @@ module PC_reg(
 );
 
 reg pre_stall;
-reg count;
 
-assign state = {count, pc_ram_addr[2:0]};
+assign state = pc_ram_addr[3:0];
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        pc <= 32'h80000000;
-        pc_ram_addr <= 32'h80000000;
+        pc <= 32'h7999999c;
+        pc_ram_addr <= 32'h7999999c;
         pc_ram_en <= 0;
-        count <= 0;
         pre_stall <= 0;
     end
     else if (stall) begin
@@ -37,29 +35,20 @@ always @(posedge clk or posedge rst) begin
         end
         pc_ram_addr <= 0;
         pc_ram_en <= 0;
-        count <= count;
     end
     else begin
         pre_stall <= 0;
         if (branch_flag) begin
             pc <= branch_addr;
             pc_ram_addr <= branch_addr;
-            count <= count;
-        end
-        else if (!count) begin
-            pc <= pc;
-            pc_ram_addr <= pc;
-            count <= 1;
         end
         else if (pre_stall) begin
             pc <= pc;
             pc_ram_addr <= pc;
-            count <= count;
         end
         else begin
             pc <= pc + 4;
             pc_ram_addr <= pc + 4;
-            count <= count;
         end
         pc_ram_en <= 1;
     end
